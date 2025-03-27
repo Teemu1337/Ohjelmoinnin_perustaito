1df60d3d5243fc748b40bfeb3f2bedcf9ca8017a
Teemu1337 <TEH00051@laurea.fi>
2025-03-27 09:57:00 +0200

    ...

---
+++ ParillinenVaiPariton.py
@@ -0,0 +1,7 @@
+luku = int(input("Syötä luku"))
+
+if luku % 2 == 0:
+    print(f"Luku {luku} on parillinen.")
+
+else: 
+    print(f"Luku {luku} on pariton.")
---
+++ Valuuttamuunnin.py
@@ -0,0 +1,7 @@
+print("*************************")
+print("*    Valuuttamuunnin    *")
+print("*************************")
+kurssi = float(input("Anna dollarin kurssi euroina:"))
+raha = float(input("Anna rahan määrä euroina:"))
+print(f"Rahan arvo on dollareina: {raha/kurssi:.2f}")
+print("*******************************")
